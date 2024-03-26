c52fab5ec468ac92585b43852012d4a0a3cd1313
NeoPato <gabrielhmsena20@gmail.com>
2024-03-26 14:15:44 -0300

    Aprendi

---
+++ landing/src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import App from './App.jsx'
+import { Tecnologies } from './Tecnologies.jsx';
+import { Contact } from './Contact.jsx';
+
+
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:  <App />,
+  },
+  {
+    path: "/tec",
+    element:  <Tecnologies />,
+  },
+  {
+    path: "/contact",
+    element:  <Contact />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
